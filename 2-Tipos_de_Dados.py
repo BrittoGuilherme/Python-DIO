2f28e8de900363b5340f7f13b15e98e0f4e75673
Guilherme Britto <gbritto258@hotmail.com>
2023-05-05 05:31:28 -0300

    Adição das aulas 2 e 3 + Ajustes
    Foram Adicionadas as aulas 2 e 3, e alguns ajustes na nomenclatura dos arquivos, para melhor organização.

---
+++ 2-Tipos_de_Dados.py
@@ -0,0 +1,12 @@
+#Dados tipo int()
+print   (2 * 6 + 5) 
+
+#Dados tipo float()
+print   (2.5 * 2 + 6.2)
+
+#Dados tipo bool()
+print   (True)
+print   (False)
+
+#Dados tipo str()
+print   ("Hello, world!")
---
+++ 3-Modo-Interativo-dir-help.py
@@ -0,0 +1,17 @@
+    #modo Interativo
+#existem duas formas para usar o modo interativo:
+#digite python no terminal para testar código diretamente no terminal
+#para testar um código direto de um arquivo existente basta digitar, python -i nome_do_arquivo.py
+#para sair do modo interativo, exit()
+
+    #function dir:
+#Sem argumentos, retorna a lista de nomes no escopo local atual. Com um argumento, retorna uma lista de atributos válidos para o objeto. Exemplo:
+dir () #Sem argumentos
+dir (100) #Com argumento
+
+    #function help
+#Invoca o sistema de ajuda integrado. É possível fazer buscas em modo interativo ou informar por parâmetro qual o nome do módulo, função, classe, método ou variável. Exemplo:
+help()
+help(100)
+#para sair , basta pressionar Q
+
